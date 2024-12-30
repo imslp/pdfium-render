af088d0d40f5da881226152650d40d035557c0b1
Tim Wei <galadiya41@gmail.com>
2024-12-30 11:43:03 +0800

    chore: update wasm

--- examples/wasm.rs
+++ examples/wasm.rs
@@ -1,146 +1,124 @@
 #[cfg(target_arch = "wasm32")]
+use js_sys::{Object, Reflect, Uint8Array};
+#[cfg(target_arch = "wasm32")]
 use pdfium_render::prelude::*;
-
+#[cfg(target_arch = "wasm32")]
+use std::{cell::RefCell, convert::TryFrom};
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
-
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen_futures::JsFuture;
 #[cfg(target_arch = "wasm32")]
 use web_sys::ImageData;
 
-// See https://github.com/ajrcarey/pdfium-render/tree/master/examples for information
-// on how to build and package this example alongside a WASM build of Pdfium, suitable
-// for running in a browser.
-
-/// Downloads the given URL, opens it as a PDF document, then logs the width and height of
-/// each page in the document, along with other document metrics, to the Javascript console.
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub async fn log_page_metrics_to_console(url: String) {
-    let pdfium = Pdfium::default();
-
-    let document = pdfium.load_pdf_from_fetch(url, None).await.unwrap();
-
-    // Output metadata and form information for the PDF file to the console.
-
-    log::info!("PDF file version: {:#?}", document.version());
-
-    log::info!("PDF metadata tags:");
-
-    document
-        .metadata()
-        .iter()
-        .enumerate()
-        .for_each(|(index, tag)| log::info!("{}: {:#?} = {}", index, tag.tag_type(), tag.value()));
-
-    let pages = document.pages();
-
-    match document.form() {
-        Some(form) => {
-            log::info!(
-                "PDF contains an embedded form of type {:#?}",
-                form.form_type()
-            );
-
-            for (key, value) in form.field_values(&pages).iter() {
-                log::info!("{:?} => {:?}", key, value);
-            }
-        }
-        None => log::info!("PDF does not contain an embedded form"),
-    };
-
-    // Report labels, boundaries, and metrics for each page to the console.
-
-    pages.iter().enumerate().for_each(|(page_index, page)| {
-        if let Some(label) = page.label() {
-            log::info!("Page {} has a label: {}", page_index, label);
-        }
-
-        log::info!(
-            "Page {} width: {}, height: {}",
-            page_index,
-            page.width().value,
-            page.height().value
-        );
-
-        for boundary in page.boundaries().iter() {
-            log::info!(
-                "Page {} has defined {:#?} box ({}, {}) - ({}, {})",
-                page_index,
-                boundary.box_type,
-                boundary.bounds.left.value,
-                boundary.bounds.top.value,
-                boundary.bounds.right.value,
-                boundary.bounds.bottom.value,
-            );
-        }
-
-        log::info!(
-            "Page {} has paper size {:#?}",
-            page_index,
-            page.paper_size()
-        );
-
-        for (link_index, link) in page.links().iter().enumerate() {
-            log::info!(
-                "Page {} link {} has action of type {:?}",
-                page_index,
-                link_index,
-                link.action().map(|action| action.action_type())
-            );
-
-            // For links that have URI actions, output the destination URI.
-
-            if let Some(action) = link.action() {
-                if let Some(uri_action) = action.as_uri_action() {
-                    log::info!("Link URI destination: {:#?}", uri_action.uri())
-                }
-            }
-        }
-
-        let text = page.text().unwrap();
-
-        for (annotation_index, annotation) in page.annotations().iter().enumerate() {
-            log::info!(
-                "Page {} annotation {} has text: {:?}, bounds: {:?}",
-                page_index,
-                annotation_index,
-                text.for_annotation(&annotation),
-                annotation.bounds()
-            );
-        }
-    });
+thread_local! {
+    static PDFIUM: RefCell<Option<Pdfium>> = RefCell::new(None);
 }
 
-/// Downloads the given URL, opens it as a PDF document, then returns the ImageData for
-/// the given page index using the given bitmap dimensions.
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub async fn get_image_data_for_page(
-    url: String,
-    index: PdfPageIndex,
-    width: Pixels,
-    height: Pixels,
-) -> ImageData {
-    Pdfium::default()
-        .load_pdf_from_fetch(url, None)
-        .await
-        .unwrap()
-        .pages()
-        .get(index)
-        .unwrap()
-        .render_with_config(
-            &PdfRenderConfig::new()
-                .set_target_size(width, height)
-                .render_form_data(true)
-                .highlight_text_form_fields(PdfColor::YELLOW.with_alpha(128))
-                .highlight_checkbox_form_fields(PdfColor::BLUE.with_alpha(128)),
-        )
-        .unwrap()
-        .as_image_data()
-        .unwrap()
+fn with_pdfium<F, R>(f: F) -> R
+where
+    F: FnOnce(&Pdfium) -> R,
+{
+    PDFIUM.with(|pdfium| {
+        let mut pdfium = pdfium.borrow_mut();
+        if pdfium.is_none() {
+            *pdfium = Some(Pdfium::default());
+        }
+        f(pdfium.as_ref().unwrap())
+    })
 }
 
-// Source files in examples/ directory are expected to always have a main() entry-point.
-// Since we're compiling to WASM, we'll never actually use this.
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub struct Document {
+    bytes: Vec<u8>,
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+impl Document {
+    #[wasm_bindgen(constructor)]
+    pub async fn new(blob: web_sys::Blob) -> Result<Document, JsValue> {
+        let array_buffer = JsFuture::from(blob.array_buffer()).await?;
+        let uint8_array = Uint8Array::new(&array_buffer);
+        let bytes = uint8_array.to_vec();
+        Ok(Document { bytes })
+    }
+
+    pub fn get_page_count(&self) -> Result<u32, JsValue> {
+        with_pdfium(|pdfium| {
+            let document = pdfium
+                .load_pdf_from_byte_vec(self.bytes.clone(), None)
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+            Ok(document.pages().len() as u32)
+        })
+    }
+
+    pub fn get_page_raw_size(&self, index: u32) -> Result<JsValue, JsValue> {
+        with_pdfium(|pdfium| {
+            let document = pdfium
+                .load_pdf_from_byte_vec(self.bytes.clone(), None)
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+
+            let page = document
+                .pages()
+                .get(u16::try_from(index).map_err(|e| JsValue::from_str(&format!("{:?}", e)))?)
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+
+            let obj = Object::new();
+            Reflect::set(
+                &obj,
+                &"width".into(),
+                &JsValue::from_f64(page.width().value as f64),
+            )?;
+            Reflect::set(
+                &obj,
+                &"height".into(),
+                &JsValue::from_f64(page.height().value as f64),
+            )?;
+            Ok(obj.into())
+        })
+    }
+
+    pub fn get_page_image_data(&self, index: u32, scale: f32) -> Result<ImageData, JsValue> {
+        with_pdfium(|pdfium| {
+            let document = pdfium
+                .load_pdf_from_byte_vec(self.bytes.clone(), None)
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+
+            let page = document
+                .pages()
+                .get(u16::try_from(index).map_err(|e| JsValue::from_str(&format!("{:?}", e)))?)
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+
+            let width = (page.width().value as f32 * scale) as u16;
+            let height = (page.height().value as f32 * scale) as u16;
+
+            let bitmap = page
+                .render_with_config(
+                    &PdfRenderConfig::new()
+                        .set_target_size(Pixels::from(width), Pixels::from(height))
+                        .render_form_data(true),
+                )
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
+
+            bitmap
+                .as_image_data()
+                .map_err(|e| JsValue::from_str(&format!("{:?}", e)))
+        })
+    }
+
+    pub fn destroy(self) {
+        drop(self);
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub async fn get_document(blob: web_sys::Blob) -> Result<Document, JsValue> {
+    Document::new(blob).await
+}
+
+// Required for wasm-pack
 #[allow(dead_code)]
 fn main() {}
---
+++ output/pdfium_render_wasm_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_document_free(a: number, b: number): void;
+export function document_new(a: number): number;
+export function document_get_page_count(a: number): number;
+export function document_get_page_raw_size(a: number, b: number): Array;
+export function document_get_page_image_data(a: number, b: number, c: number): Array;
+export function document_destroy(a: number): void;
+export function get_document(a: number): number;
+export function initialize_pdfium_render(a: number, b: number, c: number): number;
+export function read_block_from_callback_wasm(a: number, b: number, c: number, d: number): number;
+export function write_block_from_callback_wasm(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function closure775_externref_shim(a: number, b: number, c: number): void;
+export function __externref_table_dealloc(a: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __externref_table_alloc(): number;
+export function closure799_externref_shim(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
